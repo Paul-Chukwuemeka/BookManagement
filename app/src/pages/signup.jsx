ae6d60401efbfbec3b8adc306edb9536aa5482cf
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-03-19 10:32:40 +0100

    refactor: streamline Signup component code for improved readability and maintainability

--- app/src/pages/signup.jsx
+++ app/src/pages/signup.jsx
@@ -4,18 +4,14 @@ import { useNavigate } from "react-router-dom";
 const Signup = () => {
   const [userName, setUserName] = useState("");
   const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] =
-    useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
 
-  const handleSignup = async (
-    username,
-    email,
-    password
-  ) => {
+  const handleSignup = async (username, email, password) => {
+    console.log(userName, email, password);
     setError(false);
     try {
       const response = await axios.post(
@@ -26,21 +22,14 @@ const Signup = () => {
           password,
         }
       );
-      localStorage.setItem(
-        "user",
-        JSON.stringify(response.data)
-      );
+      localStorage.setItem("user", JSON.stringify(response.data));
       navigate("/");
     } catch (error) {
       setError(true);
       if (error.response) {
-        setErrorMessage(
-          error.response.data.message
-        );
+        setErrorMessage(error.response.data.message);
       } else {
-        setErrorMessage(
-          "An unexpected error occurred."
-        );
+        setErrorMessage("An unexpected error occurred.");
       }
     }
   };
@@ -51,18 +40,9 @@ const Signup = () => {
     <div className="grid grid-cols-2">
       <div className="bg-black text-white rounded-[0px_30px_30px_0px] h-screen text-5xl flex flex-col justify-around gap-2 items-center p-24  ">
         <div className="flex flex-col items-center gap-1">
-          <img
-            src="/darkLogo.png"
-            alt="Logo"
-            className="w-24"
-          />
-          <p>
-            BookWorm
-
-          </p>
-          <p className="text-3xl text-white">
-            Library
-          </p>
+          <img src="/darkLogo.png" alt="Logo" className="w-24" />
+          <p>BookWorm</p>
+          <p className="text-3xl text-white">Library</p>
         </div>
         <div className="flex flex-col items-center gap-4">
           <p className="text-white text-xl">
@@ -78,62 +58,41 @@ const Signup = () => {
       </div>
       <div className="flex flex-col justify-around items-center px-32 p-16 gap-6 ">
         <div className="flex flex-col justify-center h-2/5 items-center gap-6 ">
-          <img
-            src="/lightlogo.png"
-            alt="Logo"
-            className="w-28"
-          />
+          <img src="/lightlogo.png" alt="Logo" className="w-28" />
           <h2 className="text-4xl  gap-1 flex items-center font-bold">
             Sign In
           </h2>
-          <p className="text-lg">
-            Please provide your information to sign up
-          </p>
+          <p className="text-lg">Please provide your information to sign up</p>
         </div>
         <form
           onSubmit={async (e) => {
             e.preventDefault();
-            await handleSignup(
-              userName,
-              email,
-              password
-            );
+            await handleSignup(userName, email, password);
           }}
           className="flex w-full py-5 max-w-[450px] flex-col gap-4 justify-center items-center"
         >
-
           <input
             type="text"
             className="p-4 py-5 w-full border border-black rounded-2xl"
             placeholder="User name"
             required
-            onInput={(e) =>
-              setUserName(e.target.value)
-            }
+            onInput={(e) => setUserName(e.target.value)}
           />
           <input
             type="text"
             className="p-4 py-5 w-full border border-black rounded-2xl"
             placeholder="Email"
             required
-            onInput={(e) =>
-              setEmail(e.target.value.toLowercase())
-            }
+            onInput={(e) => setEmail((e.target.value).toLowerCase())}
           />
           <input
             type="password"
             className="p-4 py-5 w-full border border-black rounded-2xl"
             placeholder="Password"
             required
-            onInput={(e) =>
-              setPassword(e.target.value)
-            }
+            onInput={(e) => setPassword(e.target.value)}
           />
-          {error && (
-            <p className="text-red-500">
-              {errorMessage}
-            </p>
-          )}
+          {error && <p className="text-red-500">{errorMessage}</p>}
           <button
             className={`bg-black p-3 rounded-2xl text-xl py-4  font-bold w-4/5 text-white focus:outline-none ${transition}`}
           >
